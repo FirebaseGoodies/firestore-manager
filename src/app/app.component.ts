0507ed198a9c2cb40402bf50f4a391d39b066622
AXeL-dev <contact.axel.dev@gmail.com>
2019-11-10 17:31:35 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'fm-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'firestore-manager';
+}
