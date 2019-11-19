f01b833cb416383ccf4985e32cdfad67192a2c3e
AXeL-dev <contact.axel.dev@gmail.com>
2019-11-19 22:55:30 +0100

    Update polyfills.ts
    Fix `Uncaught ReferenceError: process is not defined`

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -64,3 +64,7 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
 
 // Add global to window, assigning the value of window itself.
 (window as any).global = window;
+
+(window as any).process = {
+  env: { DEBUG: undefined },
+};
