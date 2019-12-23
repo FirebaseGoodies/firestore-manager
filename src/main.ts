57b02f35b8f02537bcf3554aac68bdec47501553
AXeL-dev <contact.axel.dev@gmail.com>
2019-12-24 00:02:33 +0100

    Add missing object type
    + remove unused import on main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-import { StorageService } from './app/services/storage.service';
 
 if (environment.production) {
   enableProdMode();
