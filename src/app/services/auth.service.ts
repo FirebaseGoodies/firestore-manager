dfedba41f9017d73e34fa69edc72d0d7a988c0f5
AXeL-dev <contact.axel.dev@gmail.com>
2019-12-28 12:21:52 +0100

    Update auth.service.ts
    Code optimisation

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -5,44 +5,46 @@ import { Authentication, AuthenticationType } from '../models/auth.model';
 @Injectable()
 export class AuthService {
 
-  currentUser: any = null;
-  isSignedIn: boolean = false;
+  currentUser: firebase.User = null;
   lastError: firebase.FirebaseError = null;
 
   constructor(private afa: AngularFireAuth) {
     this.afa.auth.onAuthStateChanged((user) => {
       // console.log(user);
       this.currentUser = user;
-      this.isSignedIn = user ? true : false;
     });
+  }
+
+  isSignedIn(): boolean {
+    return !!this.currentUser;
   }
 
   signIn(authentication: Authentication): Promise<void> {
     // console.log('sign in', authentication);
     return new Promise((resolve, reject) => {
-      // Set authentication function
-      let signInFunction: Function = null;
-      switch(authentication.type) {
-        case AuthenticationType.Anonymous:
-          signInFunction = () => this.afa.auth.signInAnonymously();
-          break;
-        case AuthenticationType.EmailAndPassword:
-          if (authentication.data.email && authentication.data.password && authentication.data.email.length && authentication.data.password.length) {
-            signInFunction = () => this.afa.auth.signInWithEmailAndPassword(authentication.data.email, authentication.data.password);
-          }
-          break;
-        case AuthenticationType.Token:
-          if (authentication.data.token && authentication.data.token.length) {
-            signInFunction = () => this.afa.auth.signInWithCustomToken(authentication.data.token);
-          }
-          break;
-      }
-      // Sign in
-      if (signInFunction !== null) {
-        if (this.isSignedIn) {
-          console.log('already signed in!');
-          resolve();
-        } else {
+      if (this.isSignedIn()) {
+        console.log('already signed in!');
+        resolve();
+      } else {
+        // Set authentication function
+        let signInFunction: Function = null;
+        switch(authentication.type) {
+          case AuthenticationType.Anonymous:
+            signInFunction = () => this.afa.auth.signInAnonymously();
+            break;
+          case AuthenticationType.EmailAndPassword:
+            if (authentication.data.email && authentication.data.password && authentication.data.email.length && authentication.data.password.length) {
+              signInFunction = () => this.afa.auth.signInWithEmailAndPassword(authentication.data.email, authentication.data.password);
+            }
+            break;
+          case AuthenticationType.Token:
+            if (authentication.data.token && authentication.data.token.length) {
+              signInFunction = () => this.afa.auth.signInWithCustomToken(authentication.data.token);
+            }
+            break;
+        }
+        // Sign in
+        if (signInFunction !== null) {
           signInFunction().then(() => {
             resolve();
           }).catch((error: firebase.FirebaseError) => {
@@ -50,16 +52,16 @@ export class AuthService {
             console.error(`[${error.code}] ${error.message}`);
             reject();
           });
+        } else {
+          reject();
         }
-      } else {
-        reject();
       }
     });
   }
 
   signOut(force: boolean = false): void {
-    // console.log('sign out', this.isSignedIn);
-    if (force || this.isSignedIn) {
+    // console.log('sign out', this.isSignedIn());
+    if (force || this.isSignedIn()) {
       this.afa.auth.signOut().catch((error: firebase.FirebaseError) => {
         this.lastError = error;
         console.error(`[${error.code}] ${error.message}`);
