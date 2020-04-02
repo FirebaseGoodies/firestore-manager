36fef8b7b0fd59e3cb4f6378fc122b27ae45b328
AXeL-dev <contact.axel.dev@gmail.com>
2020-04-02 13:06:34 +0100

    Fix data change detection

--- src/app/components/explorer/explorer.component.ts
+++ src/app/components/explorer/explorer.component.ts
@@ -152,7 +152,11 @@ export class ExplorerComponent implements OnInit, OnDestroy, ComponentCanDeactiv
       // console.error(error);
     };
     this.editorOptions.onChange = () => {
-      try { } catch (error) {
+      try {
+        const data = this.editor.get();
+        JSON.stringify(data);
+        this.onEditorDataChange(data);
+      } catch (error) {
         // console.error(error);
       }
     };
